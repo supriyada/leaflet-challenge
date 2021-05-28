b2428fc91ff47856dc2013c80f6a72ea0a88a948
supriyada <supriyada@gmail.com>
2021-05-28 19:03:02 -0400

    Basic map

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "sk.eyJ1Ijoic3Vwcml5YWRhIiwiYSI6ImNrb3U2YnpoMTBmbHEybnBmdDFsbjVlcnUifQ.G_dXEh8DTEVVZ3gHkvl8mQ";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,18 @@
+var myMap = L.map("mapid", {
+    center: [37.0902, -95.7129],
+    zoom: 6
+  });
+  
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
+
+  var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
