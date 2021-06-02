bd8bb15646d2525dfcfe71b45f21bf2899183d1d
supriyada <supriyada@gmail.com>
2021-06-01 21:44:47 -0400

    Added comments

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,99 +1,83 @@
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-  
-d3.json(queryUrl).then(function(data){
-    createFeatures(data.features);    
+//Map center coordinates
+
+var myMap = L.map("mapid", {
+  center: [
+      37.09, -95.71
+  ],
+  zoom: 5,
 });
 
-function createFeatures(earthquakeData) {
+//Create a tile layer - darkmap
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "dark-v10",
+  accessToken: API_KEY
+}).addTo(myMap);
 
-    // Define a function we want to run once for each feature in the features array
-    // Give each feature a popup describing the place and time of the earthquake
-    function onEachFeature(feature, layer) {
-      layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-    }
-  
-    // Create a GeoJSON layer containing the features array on the earthquakeData object
-    // Run the onEachFeature function once for each piece of data in the array
-    var earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature
-    });
-  
-    // Sending our earthquakes layer to the createMap function
-    createMap(earthquakes);
+//URL to get the dataset
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+//Function to choose color for the depth of earthquake
+function chooseColor(mag_depth) {
+
+  switch (true) {
+      case (mag_depth < 10):
+          return "green";
+      case (mag_depth < 30):
+          return "#4287f5";
+      case (mag_depth < 50):
+          return "#fcf005";
+      case (mag_depth < 70):
+          return "#eb9234";
+      case (mag_depth < 90):
+          return "#fc6405";
+      default:
+          return "#f20202";
   }
+}
 
-  function createMap(earthquakes) {
+//Retrieve json data
+d3.json(queryUrl).then(function (data) {
+  var earthquakes = data.features;
+  console.log(earthquakes)
 
-    // Define streetmap and darkmap layers
-    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      tileSize: 512,
-      maxZoom: 18,
-      zoomOffset: -1,
-      id: "mapbox/streets-v11",
-      accessToken: API_KEY
-    });
-  
-    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "dark-v10",
-      accessToken: API_KEY
-    });
-  
-    // Define a baseMaps object to hold our base layers
-    var baseMaps = {
-      "Street Map": streetmap,
-      "Dark Map": darkmap
-    };
-  
-    // Create overlay object to hold our overlay layer
-    var overlayMaps = {
-      Earthquakes: earthquakes
-    };
-  
-    // Create our map, giving it the streetmap and earthquakes layers to display on load
-    var myMap = L.map("mapid", {
-      center: [
-        37.09, -95.71
-      ],
-      zoom: 5,
-      layers: [streetmap, earthquakes]
-    });
-  
-    // Create a layer control
-    // Pass in our baseMaps and overlayMaps
-    // Add the layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(myMap);
+  for (var i = 0; i < earthquakes.length; i++) {
 
-    // Set up the legend
+      // Set the data location property to a variable
+      var location = earthquakes[i].geometry.coordinates;
+
+      var mag_depth = location[2]
+
+      var magnitude = earthquakes[i].properties.mag;
+      //Circle markers
+      L.circleMarker([location[1], location[0]], {
+          color: "green",
+          fillColor: chooseColor(mag_depth),
+          fillOpacity: 0.5,
+          radius: magnitude * 5
+      }).bindPopup("<strong>Place: </strong>" + earthquakes[i].properties.place +
+          "<br><p><strong>Date: </strong>" + new Date(earthquakes[i].properties.time) +
+          "<br><strong>Magnitude: </strong>" + earthquakes[i].properties.mag +
+          "<br><strong>Depth: </strong>" + earthquakes[i].geometry.coordinates[2] + "</p>")
+          .addTo(myMap);
+
+  }
+  //Legend for the magnitude depth
   var legend = L.control({ position: "bottomright" });
-  legend.onAdd = function() {
-    var div = L.DomUtil.create("div", "info legend");
-    var limits = queryUrl.options.limits;
-    var colors = queryUrl.options.colors;
-    var labels = [];
 
-    // Add min & max
-    var legendInfo = "<h1>Median Income</h1>" +
-      "<div class=\"labels\">" +
-        "<div class=\"min\">" + limits[0] + "</div>" +
-        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-      "</div>";
+  legend.onAdd = function (map) {
+      var div = L.DomUtil.create("div", "legend");
+      div.innerHTML += "<h4>Earthquake depth</h4>";
+      div.innerHTML += '<i style="background: green"></i><span>-10-10</span><br>';
+      div.innerHTML += '<i style="background: #4287f5"></i><span>10-30</span><br>';
+      div.innerHTML += '<i style="background: #fcf005"></i><span>30-50</span><br>';
+      div.innerHTML += '<i style="background: #eb9234"></i><span>50-70</span><br>';
+      div.innerHTML += '<i style="background: #fc6405"></i><span>70-90</span><br>';
+      div.innerHTML += '<i style="background: #f20202"></i><span>90+</span><br>';
 
-    div.innerHTML = legendInfo;
-
-    limits.forEach(function(limit, index) {
-      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    });
-
-    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    return div;
+      return div;
   };
 
-  // Adding legend to the map
   legend.addTo(myMap);
-  }
+});
